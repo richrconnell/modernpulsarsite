f1b7109727f14a59b646b67c1f318d29a792db23
sandrkim <94028681+wosroc@users.noreply.github.com>
2022-04-28 19:35:47 -0400

    tawk changes 02

---
+++ assets/js/tawk.js
@@ -0,0 +1,12 @@
+
+
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5ef788e94a7c6258179b7552/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+
